8a19ed4940c820f3b35b19f52f721dacba81d63e
Gapjin Yang <dkvowk@naver.com>
2021-03-07 20:23:56 +0900

    Controller, Dto Test추가

--- src/test/java/com/jini/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jini/springboot/web/HelloControllerTest.java
@@ -6,11 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
@@ -27,6 +26,19 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())           // HTTP header 검증 (여기서는 200만 검증)
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto_return() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                   .param("name", name)
+                   .param("amount", String.valueOf(amount)))
+                                                                  .andExpect(status().isOk())
+                                                                  .andExpect(jsonPath("$.name", is(name)))
+                                                                  .andExpect(jsonPath("$.amount", is(amount)));
     }
 
 
---
+++ src/test/java/com/jini/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,25 @@
+package com.jini.springboot.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_fnc_test() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        // assertj 라는 테스트 검증 라이브러리의 검증 메소드
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+}
