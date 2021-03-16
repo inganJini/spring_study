f713abbda9586dc590925fec9de9c617b942d507
Gapjin Yang <dkvowk@naver.com>
2021-03-16 22:23:43 +0900

    등록 api를 위한 컨트롤러 생성 테스트 git status

---
+++ src/main/java/com/jini/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.jini.springboot.service.posts;
+
+import com.jini.springboot.domain.posts.PostsRepository;
+import com.jini.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/jini/springboot/web/PostsApiController.java
@@ -0,0 +1,22 @@
+package com.jini.springboot.web;
+
+import com.jini.springboot.service.posts.PostsService;
+import com.jini.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
+        return postsService.save(requestDto);
+    }
+
+}
